dd7696b5b498d0e13eb0a4a1af14a3b6f344df76
gtagnetagne <georges.tagnetagne@gmail.com>
2026-04-21 08:47:38 +0200

    Utilisation du local storage sur l'App

--- src/components/taskContainer.jsx
+++ src/components/taskContainer.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Footer } from "./footer/footer"
 import { Header } from "./header/header"
 import { TaskInput } from "./taskInput/taskInput"
@@ -7,6 +7,18 @@ import { TaskList } from "./taskList/taskList"
 export const TaskContainer = () => {
 
     const [tasksList, setTasksList] = useState([]);
+
+    //Ce useEffect est pour le local storage de l'App
+    useEffect(() => {
+        const storedTasks = localStorage.getItem("tasksList")
+        if (storedTasks) { setTasksList(JSON.parse(storedTasks)); }
+    }, [])
+
+        const saveTasksToLocalStorage = (tasks)=>{
+            localStorage.setItem("tasksList", JSON.stringify(tasks));  
+        }
+
+
     const nextId = useRef(1);
 
     const addTask = (title) => {
@@ -15,27 +27,32 @@ export const TaskContainer = () => {
             title,
             completed: false,
         };
-        setTasksList([...tasksList, newTask]);
+        const updatedTasks = [...tasksList, newTask];
+        setTasksList(updatedTasks);
+        saveTasksToLocalStorage (updatedTasks);
     };
 
     const editTask = (id, completedValue) => {
-        setTasksList(
-            tasksList.map((task) =>
+        const updatedTasks = tasksList.map((task) =>
                 task.id === id ? { ...task, completed: completedValue } : task
-            )
         );
+        setTasksList (updatedTasks);
+        saveTasksToLocalStorage (updatedTasks);
     };
 
     const deleteTask = (id) => {
-        setTasksList(tasksList.filter((task) => task.id !== id));
+        const updatedTasks = (tasksList.filter((task) => task.id !== id));
+        setTasksList (updatedTasks);
+        saveTasksToLocalStorage (updatedTasks);
     };
 
     const updateTask = (id, newTitle) => {
-        setTasksList(
+        const updatedTasks = 
             tasksList.map((task) =>
                 task.id === id ? { ...task, title: newTitle } : task
-            )
         );
+        setTasksList (updatedTasks);
+        saveTasksToLocalStorage (updatedTasks);
     };
 
     const completedTasks = tasksList.filter((task) => task.completed).length;
